04d76fdcc2880eb178bb1bc5656d59012e2b5670
Jhan-hab <jhanhab51@gmail.com>
2025-03-01 02:39:57 +0300

    header and footer

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Home from './pages/Home/Home';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello There</h1>
+      <Home/>
     </div>
   );
 }
---
+++ netflix-clone/src/components/Header/Header.js
@@ -0,0 +1,39 @@
+import React from "react";
+import "./header.css";
+import NetflixLogo from "../../assets/images/logo.png";
+import SearchIcon from '@mui/icons-material/Search';
+import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
+import AccountBoxIcon from '@mui/icons-material/AccountBox';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+
+const Header = () => {
+  return (
+    <div className="header-outter-container">
+      <div className="header-container">
+        <div className="header-left">
+          <ul>
+            <li>
+              <img src={NetflixLogo} width="100" alt="logo" />
+            </li>
+            <li>Home</li>
+            <li>TvShows</li>
+            <li>Movies</li>
+            <li>Latest</li>
+            <li>MyList</li>
+            <li>Browse by Languages</li>
+          </ul>
+        </div>
+        <div className="header-right">
+          <ul>
+            <li><SearchIcon /></li>
+            <li><NotificationsNoneIcon /></li>
+            <li><AccountBoxIcon /></li>
+            <li><ArrowDropDownIcon /></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
