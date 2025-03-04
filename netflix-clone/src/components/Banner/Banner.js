6c9d4860f02235b91c91ef788853ebd42ebf5017
Jhan-hab <jhanhab51@gmail.com>
2025-03-05 00:15:46 +0300

    update rows

--- netflix-clone/src/components/Banner/Banner.js
+++ netflix-clone/src/components/Banner/Banner.js
@@ -1,52 +1,49 @@
-import React, {useState, useEffect} from 'react'
-import axios from "../../utils/axios"
-import requests from "../../utils/requests"
-import "./Banner.css"
+import React, { useEffect, useState } from 'react'
+import "./Banner.css";
+import axios from "axios";
 
 const Banner = () => {
     const [movie, setMovie] = useState({});
-	useEffect(() => {
-		async function fetchData() {
-			const request = await axios.get(requests.fetchNetflixOriginals); 
-            console.log(request);
-            
-			setMovie(
-				request.data.results[
-					Math.floor(Math.random() * request.data.results.length)
-				]
-			);
-			return request;
-		}
-        fetchData();
-	}, []);
+
+    useEffect(() => {
+        (async () => {
+            try {
+                const request = await axios.get(`http://localhost:4000/api/fetchNetflixOriginals`)
+                setMovie(request.data.results[
+                    Math.floor(Math.random() * request.data.results.length)
+                ]);
+            } catch (error) {
+                console.log("error",error);
+            }
+        })()
+    }, []);
 
     function truncate(str, n) {
-        return str?.lenght > n ? str.substr(0, n - 1) + '...' : str;
+        return str?.length > n ? str.substr(0, n - 1) + '...' : str;
     }
 
-
     return (
-        <div 
-            className='banner'
+        <header
+            className="banner"
             style={{
                 backgroundSize: "cover",
-                backgroundImage: `url("https://image.tmdb.org/t/p/original${movie?.backdrop_path}")`,
+                backgroundImage: `url('https://image.tmdb.org/t/p/original${movie?.backdrop_path}')`,
                 backgroundPosition: "center",
-                backgroundRepeat: "no-repeat"
             }}
         >
-            <div className='banner-contents'>
-                <h1 className='banner-title'>
-                {movie?.title || movie?.name || movie?.original_name}
+            <div className="banner__contents">
+                <h1 className="banner__title">
+                    {movie?.title || movie?.name || movie?.original_name}
                 </h1>
-                <div className='banner-buttons'>
-                    <button className='banner-button play'>Play</button>
-                    <button className='banner-button'>My List</button>
+                <div className="banner__buttons">
+                    <button className="banner__button">Play</button>
+                    <button className="banner__button">My List</button>
                 </div>
-                <h1 className='banner-description'>{truncate(movie?.overview, 150)}</h1>
+                <h1 className="banner__description">{truncate(movie?.overview, 150)}</h1>
             </div>
-            <div className='banner-fadeBottom' />
-        </div>
-    );
-};
+            <div className="banner__fadeBottom" />
+        </header>
+    )
+}
+
 export default Banner
--- netflix-clone/src/components/Rows/RowList/RowList.js
+++ netflix-clone/src/components/Rows/RowList/RowList.js
@@ -1,20 +1,27 @@
-import React from 'react'
-import Row from '../Row/Row'
-import requests from "../../../utils/requests"
+import React from "react";
+import Row from "../Row/Row";
+import requests from "../../../utils/requests";
 
 function RowList() {
   return (
     <>
-        <Row 
-            title="NETFLIX ORIGINALS"
-            fetchUrl={requests.fetchNetflixOriginals}
-        />
-        {/* <Row />
-        <Row />
-        <Row />
-        <Row /> */}
+    
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow={true}
+      />
+      <Row title="TRENDING NOW" fetchUrl={requests.fetchTrending} />
+      <Row title="TOP RATED MOVIES" fetchUrl={requests.fetchTopRatedMovies} />
+      <Row title="ACTION MOVIES" fetchUrl={requests.fetchActionMovies} />
+      <Row title="COMEDY MOVIES" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="HORROR MOVIES" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="ROMANCE MOVIES" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="TV SHOWS" fetchUrl={requests.fetchTvShow} />
+      <Row title="DOCUMENTARIES" fetchUrl={requests.fetchDocumentaries} />
+
     </>
-  )
+  );
 }
 
-export default RowList
+export default RowList;
